b1c6507f9a095c023067aeeb16b27b5b2c79db48
sidharth-99 <sidharthv24@gmail.com>
2025-02-10 00:48:46 +0530

    event handler eg

--- introdemo/src/App.jsx
+++ introdemo/src/App.jsx
@@ -1,18 +1,19 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react';
 
 const App = () => {
-
   const [ counter, setCounter ] = useState(0)
 
-
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
-
   return (
-    <div>{counter}</div>
+    <div>
+      <div>{counter}</div>
+      <button onClick={() => setCounter(counter + 1)}>
+        plus
+      </button>
+
+      <button onClick={() => setCounter(0)}> 
+        zero
+      </button>
+    </div>
   )
 }
-
-export default App
+export default App;
