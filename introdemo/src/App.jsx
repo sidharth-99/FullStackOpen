a2f515bbfd179483f8ffa6066ffce2c556152ca7
sidharth-99 <sidharthv24@gmail.com>
2025-02-10 22:35:35 +0530

    another way

--- introdemo/src/App.jsx
+++ introdemo/src/App.jsx
@@ -1,50 +1,26 @@
 import { useState } from 'react';
 
 
-const Button = ({ onClick, text }) => (
-  //onClick naming is actually advised by react docs(can have any name for the function )
-  <button onClick={onClick}>
-    {text}
-  </button>
-)
-
-const Display = ({ counter }) => {
-  //since only returning one element, we can omit the return statement,(same as used in button)
-  return (
-    <div>{counter}</div>
-  )
-}
-
 const App = () => {
-  const [counter, setCounter] = useState(0)
+  const [clicks, setClicks] = useState({
+    left: 0, right: 0
+  })
 
-  console.log('rendering with counter value', counter)
+  const handleLeftClick = () =>
+    setClicks({ ...clicks, left: clicks.left + 1 })//don't set clicks.left = clicks.left + 1(wont work)
 
-  const increaseByOne = () => {
-
-    console.log('increasing, value before', counter)
-    setCounter(counter + 1)
-  }
-
-  const decreaseByOne = () => { 
-
-    console.log('decreasing, value before', counter)
-    setCounter(counter - 1)
-  }
-
-  const setToZero = () => {
-
-    console.log('resetting to zero, value before', counter)
-    setCounter(0)
-  }
+  
+const handleRightClick = () =>
+  setClicks({ ...clicks, right: clicks.right + 1 })
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button onClick={increaseByOne} text="plus" />
-      <Button onClick={setToZero} text="zero" />
-      <Button onClick={decreaseByOne} text="minus" />
+      {clicks.left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {clicks.right}
     </div>
   )
-} 
+}
+
 export default App;
