4a4949296a043720b079210aecabfe6eb4ccf860
sidharth-99 <sidharthv24@gmail.com>
2025-02-11 22:16:47 +0530

    1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -6,12 +6,18 @@ const Header = ({course}) => {
 
 // const Content = ({parts}) => {
 
+const Part = ({part, exercises}) => {
+  return (
+    <p>{part} {exercises}</p>
+  )
+}
+
 const Content = ({part1, exercises1, part2, exercises2, part3, exercises3}) => {
   return (
     <div>
-      <p>{part1} {exercises1}</p>
-      <p>{part2} {exercises2}</p>
-      <p>{part3} {exercises3}</p>
+      <Part part={part1} exercises={exercises1} />
+      <Part part={part2} exercises={exercises2} />
+      <Part part={part3} exercises={exercises3} />
     </div>
   )
 }
