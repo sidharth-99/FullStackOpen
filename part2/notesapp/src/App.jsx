ab6bae8ef5928a31d58fbe6fcd665d36d5506b06
sidharth-99 <sidharthv24@gmail.com>
2025-02-21 00:17:46 +0530

    important logic

--- part2/notesapp/src/App.jsx
+++ part2/notesapp/src/App.jsx
@@ -1,41 +1,53 @@
-import Note from './components/Note'
 import { useState } from 'react'
+import Note from './components/Note'
 
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes)
-  const [newNote, setNewNote] = useState('a new note...')
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true) // Initially show all notes 
 
   const addNote = (event) => {
-    event.preventDefault() //prevents the default behavior of the form of refreshing the page
+    event.preventDefault()
     const noteObject = {
       content: newNote,
-      important: Math.random() < 0.5,
+      important: Math.random() > 0.5,
       id: String(notes.length + 1),
     }
-  
+
     setNotes(notes.concat(noteObject))
     setNewNote('')
-  }  
+  }
 
   const handleNoteChange = (event) => {
-    console.log(event.target.value)
     setNewNote(event.target.value)
   }
+
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important)
+
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
       <ul>
-        {notes.map(note => 
+        {notesToShow.map((note) => (
           <Note key={note.id} note={note} />
-        )}
+        ))}
       </ul>
-
       <form onSubmit={addNote}>
         <input value={newNote} onChange={handleNoteChange} />
         <button type="submit">save</button>
-      </form>   
+      </form>
     </div>
   )
 }
 
 export default App
+//Basically neNote is the state that holds the value of the input field. 
+//handleNoteChange is the event handler that updates the state of newNote whenever the input field changes. 
+//addNote is the event handler that adds a new note to the notes state whenever the form is submitted. 
+//The notesToShow variable is used to determine which notes to display based on the value of the showAll state. 
+//Finally, the App component renders the list of notes, a form for adding new notes, and a button for toggling between showing all notes and showing only important notes.
