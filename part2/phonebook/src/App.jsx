23f170582d3c3a9281a59c5651a2ef787632d4f2
sidharth-99 <sidharthv24@gmail.com>
2025-02-22 18:55:48 +0530

    2.10

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,12 +1,49 @@
 import { useState } from 'react'
 
+const Filter = ({ newSearchInput, handleSeacrhInputChange }) => {
+  return (
+    <div>
+      Filter shown with<input value={newSearchInput} onChange={handleSeacrhInputChange}/>
+    </div>
+  )
+}
+
+const PersonForm = ({ addPerson, newName, handleNameChange, newNumber, handleNumberChange }) => {
+  return (<form onSubmit={addPerson}>
+    <div>
+      name: <input value={newName} onChange={handleNameChange}/>
+      <br/>
+      number: <input value={newNumber} onChange={handleNumberChange}/>
+    </div>
+    <div>
+      <button type="submit">add</button>
+    </div>
+  </form>)
+  }
+
+const Persons = ({showPersons}) => {
+  return (
+    <div>
+      {showPersons.map((person) => (
+        <div key={person.name}>
+          {person.name} {person.number}
+        </div>
+      ))}
+    </div>
+  )
+}
+    
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
-
+  const [newSearchInput, setnewSearchInput] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -30,26 +67,22 @@ const App = () => {
     setNewNumber(event.target.value)
   } 
 
+  const handleSeacrhInputChange = (event) => {
+    setnewSearchInput(event.target.value)
+  }
+
+  const showPersons = newSearchInput === ''// if search input is empty, show all persons
+    ? persons
+    : persons.filter((person) => person.name.toLowerCase().includes(newSearchInput.toLowerCase()))
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange}/>
-          <br/>
-          number: <input value={newNumber} onChange={handleNumberChange}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <Filter newSearchInput={newSearchInput} handleSeacrhInputChange={handleSeacrhInputChange}/>
+      <h2>Add a new</h2>
+      <PersonForm addPerson={addPerson} newName={newName} handleNameChange={handleNameChange} newNumber={newNumber} handleNumberChange={handleNumberChange}/>
       <h2>Numbers</h2>
-      {persons.map((person) => 
-        <div key={person.name}>
-          {person.name} {person.number}
-        </div>
-      )}
+      <Persons showPersons={showPersons}/>
     </div>
   )
 }
